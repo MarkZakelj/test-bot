0ddf189f2e62da5879a54f6e69802cae14a32936
MarkZakelj <markzakelj@gmail.com>
2024-05-17 11:22:30 +0200

    update

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // Enter the code in the URL - https://app.sli.do/event/EVENT_CODE/live/questions
-  event_code: '',
+  event_code: '85ZF2aNYLHiAc1xUBo3UBB',
   // Which question you want to boost?
-  question_id: '',
+  question_id: '0',
   // How many likes you want?
-  like_wish: 5,
+  like_wish: 400,
 
   // You can visually watch how the script is working
   // default is false, you can see the logs in terminal
--- index.js
+++ index.js
@@ -22,14 +22,14 @@ const puppeteer_config = {
   const page = await browser.newPage()
 
   for (let index = 0; index < like_wish; index++) {
-    await page.goto(`https://app.sli.do/event/${event_code}/live/questions`)
+    await page.goto(`https://app.sli.do/event/${event_code}/live/polls`)
     await page.setViewport({ width: 1200, height: 800 })
 
     await page.waitForSelector('.app__content__body')
 
     console.log('🔸 Looking for question...')
 
-    await page.waitForSelector('.question-list__container')
+    await page.waitForSelector('.poll-question-options')
 
     if (config.safe_wait) {
       await page.evaluate(async () => {
@@ -41,14 +41,28 @@ const puppeteer_config = {
 
     autoScroll(page)
 
-    await page.waitForSelector(`[data-qid="${qid}"] button`)
+    await page.waitForSelector(`#live-tabpanel-polls > div > div > div.poll--survey > form > div:nth-child(1) > div:nth-child(1) > div > div.poll-question__body-container > div > div.poll-question__body > div > label:nth-child(1) > span.MuiButtonBase-root.MuiIconButton-root.jss2.MuiRadio-root.MuiRadio-colorSecondary.MuiIconButton-colorSecondary`)
 
     console.log('🔹 Question found')
-
-    const btn = await page.$(`[data-qid="${qid}"] button`)
+    await page.waitForTimeout(Math.floor(Math.random() * 800) + 500)
+    const btn = await page.$(`#live-tabpanel-polls > div > div > div.poll--survey > form > div:nth-child(1) > div:nth-child(1) > div > div.poll-question__body-container > div > div.poll-question__body > div > label:nth-child(1) > span.MuiButtonBase-root.MuiIconButton-root.jss2.MuiRadio-root.MuiRadio-colorSecondary.MuiIconButton-colorSecondary`)
     await btn.evaluate((btn) => btn.click())
 
-    await page.waitForResponse(`https://app.sli.do/eu1/api/v0.5/events/${uuid}/questions/${qid}/like`)
+    // select random number from 1 to 3
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNumber)
+    await page.waitForSelector(`#live-tabpanel-polls > div > div > div.poll--survey > form > div:nth-child(1) > div:nth-child(2) > div > div.poll-question__body-container > div > div.poll-question__body > div > label:nth-child(${randomNumber}) > span.MuiButtonBase-root.MuiIconButton-root.jss2.MuiRadio-root.MuiRadio-colorSecondary.MuiIconButton-colorSecondary`)
+    const btn2 = await page.$(`#live-tabpanel-polls > div > div > div.poll--survey > form > div:nth-child(1) > div:nth-child(2) > div > div.poll-question__body-container > div > div.poll-question__body > div > label:nth-child(${randomNumber}) > span.MuiButtonBase-root.MuiIconButton-root.jss2.MuiRadio-root.MuiRadio-colorSecondary.MuiIconButton-colorSecondary`)
+    await page.waitForTimeout(Math.floor(Math.random() * 800) + 500)
+    await btn2.evaluate((btn2) => btn2.click())
+
+    await page.waitForTimeout(Math.floor(Math.random() * 1000) + 1000)
+    // await page.waitForResponse(`https://app.sli.do/eu1/api/v0.5/events/${uuid}/questions/${qid}/like`)
+    const submit_btn_selector = '.poll__btn-submit';
+    await page.waitForSelector(submit_btn_selector);
+    const submit_btn = await page.$(submit_btn_selector);
+    await submit_btn.evaluate((submit_btn) => submit_btn.click());
+
 
     console.log(`💚 Liked! (${index + 1}/${like_wish})`)
 
@@ -66,8 +80,11 @@ const puppeteer_config = {
       name: 'Slido.EventAuthTokens',
       domain: 'app.sli.do',
     })
+    console.log('🔹 Waiting random time...')
+    await page.waitForTimeout(Math.floor(Math.random() * 800) + 500)
+    console.log('🔹 Done!')
   }
-
+  // wait random time
   await browser.close()
 })()
 
